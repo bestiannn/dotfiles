bcecaed539df7ea12ae3c7b860f31e3b01618d6a
bestiannn <rebolledocofrebastian@gmail.com>
2023-02-25 00:49:44 -0300

    autoinstall packages script

---
+++ install-packages.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo "yay -S qtile alacritty rofi pavucontrol zsh \
+nitrogen pamixer playerctl brightnessctl \
+network-manager-applet numlockx scrot feh \
+zathura qimgv-git picom-jonaburg-git \
+thunar xarchiver thunar-archive-plugin tumbler glib2 gvfs \
+dunst ttf-cascadia-code noto-fonts-emoji ttf-mononoki-nerd ttf-roboto-mono-nerd
+lightdm lightdm-gtk-greeter"
+
+yay -S qtile alacritty rofi pavucontrol zsh \
+nitrogen pamixer playerctl brightnessctl \
+network-manager-applet numlockx scrot feh \
+zathura qimgv-git picom-jonaburg-git \
+thunar xarchiver thunar-archive-plugin tumbler glib2 gvfs \
+dunst ttf-cascadia-code noto-fonts-emoji ttf-mononoki-nerd ttf-roboto-mono-nerd
+lightdm lightdm-gtk-greeter
