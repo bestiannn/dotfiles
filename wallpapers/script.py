1d7b6c695127bdbb0986101867c8e4137fa07462
bestiannn <rebolledocofrebastian@gmail.com>
2023-03-09 01:20:18 -0300

    script readme generator fixed

--- wallpapers/script.py
+++ wallpapers/script.py
@@ -5,4 +5,4 @@ directory = os.path.dirname(os.path.abspath(__file__))
 for root, dirs, files in os.walk(directory):
     for file in files:
         if not (file.endswith('.py') or file.endswith('.md')):
-            print(f'![{}]({file})')
+            print(f'![]({file})')
